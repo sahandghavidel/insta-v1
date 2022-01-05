335ff48ada3976a0d9a74c30e4a2e823de77d003
Sahandgha <sahand.gha@gmail.com>
2022-01-05 11:20:48 +1000

    add instageram icons on the left side of the header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,24 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <div>
+    <div className="flex justify-between bg-white max-w-6xl">
       {/* Left */}
-
+      <div className="cursor-pointer relative hidden lg:inline-grid w-24 h-24">
+        <Image
+          layout="fill"
+          src="https://www.androidguys.com/wp-content/uploads/2015/04/insta.png"
+          className="object-contain"
+        />
+      </div>
+      <div className="cursor-pointer relative lg:hidden w-10 h-10">
+        <Image
+          layout="fill"
+          src="https://static.xx.fbcdn.net/assets/?revision=816167972411634&name=desktop-instagram-gradient-logo&density=1"
+          className="object-contain"
+        />
+      </div>
+      <h1>Hello</h1>
       {/* Middle */}
 
       {/* Right */}
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["www.androidguys.com", "static.xx.fbcdn.net"],
+  },
+};
