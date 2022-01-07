06341acbbd87271cf1948b46b1bad452c228e239
Sahandgha <sahand.gha@gmail.com>
2022-01-08 08:29:35 +1000

    create the haeder of the post

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,33 @@
-export default function Post() {
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+export default function Post({ username, username, userImg, img, caption }) {
   return (
-    <div>
-      <h1>Post</h1>
+    <div className="bg-white my-7 border rounded-sm">
+      {/* Header */}
+      <div className="flex items-center p-5">
+        <img
+          className="rounded-full h-12 object-cover border p-1 mr-3"
+          src={userImg}
+          alt={"Image of " + username}
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      {/* img */}
+
+      {/* Buttons */}
+
+      {/* comments */}
+
+      {/* input box */}
     </div>
   );
 }
