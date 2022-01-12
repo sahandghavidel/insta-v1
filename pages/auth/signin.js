667e294eb6e47b9067893dd52e82512afd2fbdd3
Sahandgha <sahand.gha@gmail.com>
2022-01-12 11:33:46 +1000

    craete signin page and complete google cloud

---
+++ pages/auth/signin.js
@@ -0,0 +1,23 @@
+import { getProviders, signIn } from "next-auth/react"
+
+export default function SignIn({ providers }) {
+  return (
+    <>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
+  )
+}
+
+// This is the recommended way for Next.js 9.3 or newer
+export async function getServerSideProps(context) {
+  const providers = await getProviders()
+  return {
+    props: { providers },
+  }
+}
