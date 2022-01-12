95bc789812ab8b158e9611f6a931931a0d32f21a
Sahandgha <sahand.gha@gmail.com>
2022-01-13 09:06:38 +1000

    modify miniprofile section with session

--- components/Feed.js
+++ components/Feed.js
@@ -1,26 +1,29 @@
+import { useSession } from "next-auth/react";
 import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
 import Suggestions from "./Suggestions";
 
 export default function Feed() {
+  const { data: session } = useSession();
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+    <main className={`grid grid-cols-1 md:grid-cols-2 max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${!session && "!grid-cols-1 !max-w-3xl"}`}>
       <section className="col-span-2">
         {/* Stories */}
         <Stories />
         {/* Posts */}
         <Posts />
-      </section> 
-
-      <section className="hidden xl:inline-grid md:col-span-1">
-        {/* Mini Profile */}
-        <div className="fixed w-[380px]">
-          <MiniProfile />
-          <Suggestions />
-        </div>
-        {/* Suggestions */}
       </section>
+      {session && (
+        <section className="hidden xl:inline-grid md:col-span-1">
+          {/* Mini Profile */}
+          <div className="fixed w-[380px]">
+            <MiniProfile />
+            <Suggestions />
+          </div>
+          {/* Suggestions */}
+        </section>
+      )}
     </main>
   );
 }
--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,16 +1,22 @@
+import { signOut, useSession } from "next-auth/react";
+
 export default function MiniProfile() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="h-16 rounded-full border p-[2px]"
-        src="https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048"
+        src={session?.user.image}
         alt="userImage"
       />
       <div className="flex-1 ml-4">
-        <h2 className="font-bold">codewithsahand</h2>
+        <h2 className="font-bold">{session?.user.username}</h2>
         <h3 className="text-sm text-gray-400 ">Welcome to Instageram</h3>
       </div>
-      <button className="font-semibold text-blue-400 text-sm">Sing out</button>
+      <button onClick={signOut} className="font-semibold text-blue-400 text-sm">
+        Sing out
+      </button>
     </div>
   );
 }
