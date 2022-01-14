97737766f953bc96950f45972f559160b18126bc
Sahandgha <sahand.gha@gmail.com>
2022-01-15 08:56:56 +1000

    get the posts from firebase and show them in the feed section

--- components/Posts.js
+++ components/Posts.js
@@ -1,34 +1,30 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Post from "./Post";
 
 export default function Posts() {
-  const posts = [
-    {
-      id: "123",
-      username: "codewithsahand",
-      userImg:
-        "https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048",
-      img: "https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/WOPA160517_D056-resized.jpg?crop=864,0,1728,2304&wid=600&hei=800&scl=2.88",
-      caption: "Thanks for watching",
-    },
-    {
-      id: "124",
-      username: "codewithsahand",
-      userImg:
-        "https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048",
-      img: "https://cdn-prod.medicalnewstoday.com/content/images/articles/325/325466/man-walking-dog.jpg",
-      caption: "Thank you guys",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const unsubcribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+
+    return unsubcribe;
+  }, [db]);
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profleImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
